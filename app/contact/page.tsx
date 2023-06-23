fdbfda3fd6dabe367d1510b5ae1f4be106fdef88
h.oda <62193961+hiroic-works@users.noreply.github.com>
2023-06-23 22:57:37 +0900

    refactor: お問い合わせページ整理、不要アセット削除

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from "next";
-import Image from "next/image";
 import ContactForm from "@/app/components/contact-form";
+import BaseContainer from "@/app/components/base-container";
 
 export const metadata: Metadata = {
   title: "Contact",
@@ -17,24 +17,13 @@ export const metadata: Metadata = {
 
 export default function Contact() {
   return (
-    <section className="text-gray-700">
-      <div className="hidden sm:block h-80 overflow-hidden">
-        <Image
-          className="w-full object-cover object-center"
-          src="/contact-header.jpg"
-          width={2048}
-          height={1360}
-          alt=""
-        />
+    <BaseContainer>
+      <h1 className="flex flex-col text-center w-full sm:text-4xl text-3xl font-medium mb-6">
+        お問い合わせ
+      </h1>
+      <div className="lg:w-1/2 md:w-2/3 mx-auto">
+        <ContactForm />
       </div>
-      <div className="container px-5 py-12 sm:py-16 mx-auto flex flex-col">
-        <h1 className="flex flex-col text-center w-full sm:text-4xl text-3xl font-medium mb-6">
-          お問い合わせ
-        </h1>
-        <div className="lg:w-1/2 md:w-2/3 mx-auto">
-          <ContactForm />
-        </div>
-      </div>
-    </section>
+    </BaseContainer>
   );
 }
