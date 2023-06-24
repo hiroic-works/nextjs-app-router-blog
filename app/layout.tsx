0f19714a196c618c14a57292e007d9fc6e9289b6
h.oda <62193961+hiroic-works@users.noreply.github.com>
2023-06-24 23:20:11 +0900

    style: フッターを下部に固定

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,9 +49,9 @@ export default function RootLayout({
       <head>
         <link rel="mask-icon" href="/icon.svg" />
       </head>
-      <body>
+      <body className="flex flex-col min-h-screen">
         <Header />
-        <main>{children}</main>
+        <main className="flex-1">{children}</main>
         <Footer />
       </body>
     </html>
